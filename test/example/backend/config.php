e70c5e95f55ee391b649b5bbd97f127ad148ed81
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-08-06 23:48:38 +0700

    Changed for test only

--- test/example/backend/config.php
+++ test/example/backend/config.php
@@ -1,7 +1,7 @@
 <?php 
             //Configurations
-            $config['title'] = 'reSlim'; //Your title website
+            $config['title'] = 'reSlim Tuber'; //Your title website
             $config['email'] = 'youremail@gmail.com'; //Your default email
-            $config['basepath'] = 'http://localhost:1337/reSlim/test/example'; //Your folder website
-            $config['api'] = 'http://localhost:1337/reSlim/src/api'; //Your folder rest api
-            $config['apikey'] = ''; //Your api key, you can leave this blank and fill this later
+            $config['basepath'] = 'http://localhost:1337/reSlim-tuber/test/example/backend'; //Your folder website
+            $config['api'] = 'http://localhost:1337/reSlim-tuber/src/api'; //Your folder rest api
+            $config['apikey'] = '1vvyhfz3RtubHk4qWstEzMjhZOfkOHMjSK1uOQeZR'; //Your api key, you can leave this blank and fill this later
