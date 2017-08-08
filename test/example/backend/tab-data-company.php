a96055f5f296d805fee5988b7ec4405825c9cd26
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-08-08 16:19:33 +0700

    Add page data company

---
+++ test/example/backend/tab-data-company.php
@@ -0,0 +1,301 @@
+<?php 
+//Validation url param
+$search = filter_var((empty($_GET['search'])?'':$_GET['search']),FILTER_SANITIZE_STRING);
+$page = filter_var((empty($_GET['page'])?'1':$_GET['page']),FILTER_SANITIZE_STRING);
+$itemsperpage = filter_var((empty($_GET['itemsperpage'])?'10':$_GET['itemsperpage']),FILTER_SANITIZE_STRING);
+?>
+<div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <form method="get" action="<?php $_SERVER['PHP_SELF'].'?search='.filter_var($search,FILTER_SANITIZE_STRING)?>">
+                        <div class="col-lg-10 col-md-9 col-sm-9 col-xs-12">
+                            <div class="form-group">
+                                <input name="search" type="text" placeholder="Search here..." class="form-control border-input" value="<?php echo $search?>">
+                            </div>
+                            <div class="form-group hidden">
+                                <input name="m" type="text" class="form-control border-input" value="8" hidden>
+                                <input name="page" type="text" class="form-control border-input" value="1" hidden>
+                                <input name="itemsperpage" type="text" class="form-control border-input" value="10" hidden>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-2">
+                            <div class="form-group">
+                                <button name="submitsearch" type="submit" class="btn btn-fill btn-wd ">Search</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div><hr>
+            <div class="container-fluid">
+                <div class="row">
+                    <?php 
+                        if (isset($_POST['submitnewcompany']))
+                        {
+                            $post_array = array(
+                                'Username' => $datalogin['username'],
+                                'Token' => $datalogin['token'],
+                                'Name' => filter_var($_POST['name'],FILTER_SANITIZE_STRING),
+                                'Address' => filter_var($_POST['address'],FILTER_SANITIZE_STRING),
+                                'Phone' => filter_var($_POST['phone'],FILTER_SANITIZE_STRING),
+                                'Email' => filter_var($_POST['email'],FILTER_SANITIZE_STRING),
+                                'Website' => filter_var($_POST['website'],FILTER_SANITIZE_STRING)
+                            );
+                            Core::processCreate(Core::getInstance()->api.'/ads/company/new',$post_array,'Company');
+                        }
+                    ?>
+                    <!-- Start Modal -->
+                        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                          <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <h4 class="modal-title" id="myModalLabel">Add new Company</h4>
+                              </div>
+                              <form method="post" action="<?php $_SERVER['PHP_SELF']?>">
+                              <div class="modal-body">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Name</label>
+                                            <input name="name" type="text" placeholder="Input the company name here..." maxlength="50" class="form-control border-input" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Address</label>
+                                            <textarea name="address" rows="3" type="text" placeholder="Input the address here..." maxlength="250" class="form-control border-input" required></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Phone</label>
+                                            <input name="phone" type="text" placeholder="Input the phone here..." maxlength="15" class="form-control border-input" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Email</label>
+                                            <input name="email" type="text" placeholder="Input the email here..." maxlength="50" class="form-control border-input" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Website</label>
+                                            <input name="website" type="text" placeholder="Input the website here..." maxlength="50" class="form-control border-input" required>
+                                        </div>
+                                    </div>
+                                </div>
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                                <button type="submit" name="submitnewcompany" class="btn btn-primary">Submit</button>
+                              </div>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+                    <!-- End Modal -->
+                    <div class="col-md-12">
+                        <div class="card card-plain">
+                            <div class="header">
+                                <button type="submit" class="btn btn-wd" data-toggle="modal" data-target="#myModal">Add new Company</button>
+                            </div>
+                        </div>
+                    </div>
+<?php 
+    if (isset($_POST['submitupdatecompany'.(empty($_POST['companyid'])?'':$_POST['companyid'])])){
+        $post_array = array(
+            'Username' => $datalogin['username'],
+            'Token' => $datalogin['token'],
+            'Name' => $_POST['name'],
+            'Address' => $_POST['address'],
+            'Phone' => $_POST['phone'],
+            'Email' => $_POST['email'],
+            'Website' => $_POST['website'],
+            'StatusID' => $_POST['status'],
+            'CompanyID' => $_POST['companyid'],
+        );
+        Core::processUpdate(Core::getInstance()->api.'/ads/company/update',$post_array,'Company');
+    }
+                    
+    if (isset($_POST['submitdeletecompany'.(empty($_POST['companyid'])?'':$_POST['companyid'])])){
+        $post_array = array(
+            'Username' => $datalogin['username'],
+            'Token' => $datalogin['token'],
+            'CompanyID' => $_POST['companyid']
+        );
+        Core::processDelete(Core::getInstance()->api.'/ads/company/delete',$post_array,'Company');
+    }
+
+    $url = Core::getInstance()->api.'/ads/company/data/search/'.$datalogin['username'].'/'.$datalogin['token'].'/'.$page.'/'.$itemsperpage.'/?query='.rawurlencode($search);
+    $data = json_decode(Core::execGetRequest($url));
+
+    // Data Status
+    $urlstatus = Core::getInstance()->api.'/ads/status/active/'.$datalogin['token'];
+    $datastatus = json_decode(Core::execGetRequest($urlstatus));
+
+    if (!empty($data))
+        {
+            if ($data->{'status'} == "success")
+            {
+                echo '<div class="col-md-12">
+                        <div class="card card-plain">
+                            <div class="header">
+                                <h4 class="title text-uppercase">Data Company</h4>
+                                <p class="category">Message: '.$data->{'message'}.'<br>
+                                Shows no: '.$data->metadata->{'number_item_first'}.' - '.$data->metadata->{'number_item_last'}.' from total data: '.$data->metadata->{'records_total'}.'</p>
+                                    <div class="dropdown">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+		    				    			<p><i class="ti-zip"></i> Export Data <b class="caret"></b></p>
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'excel\',escape:\'false\'});">Export XLS</a></li>
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'doc\',escape:\'false\'});">Export DOC</a></li>
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'txt\',escape:\'false\'});">Export TXT</a></li>
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'csv\',escape:\'false\'});">Export CSV</a></li>
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'pdf\',pdfFontSize:\'7\',escape:\'false\'});">Export PDF</a></li>
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'sql\'});">Export SQL</a></li>
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'xml\',escape:\'false\'});">Export XML</a></li>
+                                            <li><a href="#" onClick ="$(\'#export\').tableExport({type:\'json\',escape:\'false\'});">Export JSON</a></li>
+                                        </ul>
+                                    </div>
+                            </div>
+                            <div class="content table-responsive table-full-width">
+                                <table id="export" class="table table-striped">
+                                    <thead>
+                                        <th>No</th>
+                                    	<th>Company ID</th>
+                                    	<th>Name</th>
+                                    	<th>Address</th>
+                                        <th>Phone</th>
+                                        <th>Email</th>
+                                        <th>Website</th>
+                                        <th>Status</th>
+                                        <th>Created</th>
+                                        <th>Updated at</th>
+                                        <th>Updated by</th>
+                                        <th>Manage</th>
+                                    </thead>
+                                    <tbody>';
+                $n=$data->metadata->{'number_item_first'};
+                foreach ($data->results as $name => $value) 
+	            {
+                    echo '<tr>';
+                    echo '<td>' . $n++ .'</td>';
+                    echo '<td>' . $value->{'CompanyID'} .'</td>';
+			        echo '<td>' . $value->{'Name'} .'</td>';
+        			echo '<td>' . $value->{'Address'} .'</td>';
+                	echo '<td>' . $value->{'Phone'} .'</td>';
+                    echo '<td>' . $value->{'Email'} .'</td>';
+                    echo '<td>' . $value->{'Website'} .'</td>';
+                    echo '<td>' . $value->{'Status'} .'</td>';
+                	echo '<td>' . $value->{'Created_at'} .'</td>';
+            	    echo '<td>' . $value->{'Updated_at'} .'</td>';
+                    echo '<td>' . $value->{'Updated_by'} .'</td>';
+                    echo '<td><a href="#" data-toggle="modal" data-target="#'.$value->{'CompanyID'}.'"><i class="ti-pencil"></i> Edit</a></td>';
+	    	    	echo '</tr>';              
+                }
+                echo '</tbody>
+                </table>';
+
+                echo '</div>
+                </div>';
+
+                $pagination = new Pagination;
+                echo $pagination->makePagination($data,$_SERVER['PHP_SELF'].'?m=8&search='.rawurlencode($search));
+                
+                echo '</div>';
+                foreach ($data->results as $name=>$value){
+                    echo '<!-- Start Modal -->
+                        <div class="modal fade" id="'.$value->{'CompanyID'}.'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                          <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <h4 class="modal-title" id="myModalLabel">Add new Company</h4>
+                              </div>
+                              <form method="post" action="'.$_SERVER['PHP_SELF'].'?m=8&page='.$page.'&itemsperpage='.$itemsperpage.'&search='.$search.'">
+                              <div class="modal-body">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Company ID</label>
+                                            <input name="companyid" type="text" placeholder="Input the company id here..." class="form-control border-input" value="'.$value->{'CompanyID'}.'" readonly>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Name</label>
+                                            <input name="name" type="text" placeholder="Input the company name here..." class="form-control border-input" maxlength="50" value="'.$value->{'Name'}.'" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Address</label>
+                                            <textarea name="address" rows="3" type="text" placeholder="Input the address here..." class="form-control border-input" maxlength="250" required>'.$value->{'Address'}.'</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Phone</label>
+                                            <input name="phone" type="text" placeholder="Input the phone here..." class="form-control border-input" maxlength="15" value="'.$value->{'Phone'}.'" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Email</label>
+                                            <input name="email" type="text" placeholder="Input the email here..." class="form-control border-input" maxlength="50" value="'.$value->{'Email'}.'" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Website</label>
+                                            <input name="website" type="text" placeholder="Input the website here..." class="form-control border-input" maxlength="50" value="'.$value->{'Website'}.'" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Status</label>
+                                            <select name="status" type="text" style=\'max-height:200px; overflow-y:scroll; overflow-x:hidden;\' class="form-control border-input">';
+                                                if (!empty($datastatus)) {
+                                                            foreach ($datastatus->results as $name => $valuestatus) {
+                                                                echo '<option value="'.$valuestatus->{'StatusID'}.'" '.(($valuestatus->{'StatusID'} == $value->{'StatusID'})?'selected':'').'>'.$valuestatus->{'Status'}.'</option>';
+                                                            }
+                                                        }
+                                                    echo '</select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group hidden">
+                                    <input name="CompanyID" type="text" class="form-control border-input" value="'.$value->{'CompanyID'}.'" hidden>
+                                </div>
+                              </div>
+                              <div class="modal-footer">
+                                <button type="submit" name="submitdeletecompany'.$value->{'CompanyID'}.'" class="btn btn-danger pull-left">Delete</button>
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                                <button type="submit" name="submitupdatecompany'.$value->{'CompanyID'}.'" class="btn btn-primary">Update</button>
+                              </div>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+                    <!-- End Modal -->';
+                }
+            }
+            else
+            {
+                echo '<div class="col-md-12">
+                        <div class="card card-plain">
+                            <div class="header">
+                                <h4 class="title">Message: '.$data->{'message'}.'</h4>
+                            </div>
+                        </div>
+                    </div>';
+            } 
+        }
+?>
+                            </div>
+                        </div>
+                    </div>
+
+                
