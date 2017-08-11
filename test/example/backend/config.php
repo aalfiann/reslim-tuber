cec4f2e6a81e9d8dc34022d41ce3731349b19c62
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-08-11 19:55:23 +0700

    - Update config for tester

--- test/example/backend/config.php
+++ test/example/backend/config.php
@@ -5,6 +5,6 @@
             $config['basepath'] = 'http://localhost:1337/reSlim-tuber/test/example/backend'; //Your folder backend website
             $config['homepath'] = 'http://localhost:1337/reSlim-tuber/test/example/frontend'; //Your folder frontend website
             $config['api'] = 'http://localhost:1337/reSlim-tuber/src/api'; //Your folder rest api
-            $config['apikey'] = '1vvyhfz3RtubHk4qWstEzMjhZOfkRDhlJTWrVxEil'; //Your api key, you can leave this blank and fill this later
-            $config['disqus'] = 'ommov'; //Your disqus username, you can leave this blank and fill this later
-            $config['sharethis'] = ''; //Your sharethis key, you can leave this blank and fill this later
+            $config['apikey'] = '1vvyhfz3RtubHk4qWstEzMjhZOfqK6ggIEKcnWC8e'; //Your api key, you can leave this blank and fill this later
+            $config['disqus'] = 'omovv'; //Your disqus username, you can leave this blank and fill this later
+            $config['sharethis'] = '598da1b6ea00a30012ce67a0'; //Your sharethis key, you can leave this blank and fill this later
