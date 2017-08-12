767a0235ddeeda995268b645ef71fdb1e153cec7
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-08-12 15:27:49 +0700

    - Add auto discoverable sitemap

--- test/example/frontend/global-meta.php
+++ test/example/frontend/global-meta.php
@@ -1,4 +1,5 @@
     <link rel="canonical" href="<?php echo (isset($_SERVER['HTTPS']) ? 'https' : 'http') . '://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>" />
+    <link rel="alternate" type="application/rss+xml" title="RSS Feed for <?php echo Core::getInstance()->title?>" href="rss.php" />
 
     <!-- Bootstrap core CSS -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
