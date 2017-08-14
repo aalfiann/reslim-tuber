0eff05ae3c74a61ced49cc6caa60d8effaccfc24
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-08-14 10:50:09 +0700

    Change value config for test purpose

--- test/example/backend/config.php
+++ test/example/backend/config.php
@@ -1,12 +1,14 @@
 <?php 
             //Configurations
             $config['title'] = 'reSlim Tuber'; //Your title website
+            $config['keyword'] = 'Nonton, Streaming, online, film, movie, gratis, subtitle, indonesia'; //Your keyword website
+            $config['description'] = 'Nonton atau streaming online film gratis subtitle indonesia dan english'; //Your description website
             $config['email'] = 'youremail@gmail.com'; //Your default email
             $config['basepath'] = 'http://localhost:1337/reSlim-tuber/test/example/backend'; //Your folder backend website
             $config['homepath'] = 'http://localhost:1337/reSlim-tuber/test/example'; //Your folder frontend website
             $config['api'] = 'http://localhost:1337/reSlim-tuber/src/api'; //Your folder rest api
             $config['apikey'] = '1vvyhfz3RtubHk4qWstEzMjhZOfqK6ggIEKcnWC8e'; //Your api key, you can leave this blank and fill this later
-            $config['disqus'] = ''; //Your disqus username, you can leave this blank and fill this later
+            $config['disqus'] = 'omovv'; //Your disqus username, you can leave this blank and fill this later
             $config['sharethis'] = '598da1b6ea00a30012ce67a0'; //Your sharethis key, you can leave this blank and fill this later
             $config['facebook'] = ''; //Your facebook page, you can leave this blank and fill this later
             $config['twitter'] = ''; //Your twitter page, you can leave this blank and fill this later
