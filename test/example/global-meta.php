87fdf90d7394efdb39d3f5e2e21c566c63ff917d
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-08-14 16:48:43 +0700

    - Add Google Webmaster - Add Bing Webmaster - Add Yandex Webmaster

--- test/example/global-meta.php
+++ test/example/global-meta.php
@@ -1,5 +1,8 @@
     <link rel="canonical" href="<?php echo (isset($_SERVER['HTTPS']) ? 'https' : 'http') . '://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>" />
     <link rel="alternate" type="application/rss+xml" title="RSS Feed for <?php echo Core::getInstance()->title?>" href="rss.php" />
+    <meta name="google-site-verification" content="<?php echo Core::getInstance()->googlewebmaster?>" />
+    <meta name="msvalidate.01" content="<?php echo Core::getInstance()->bingwebmaster?>"/>
+    <meta name="yandex-verification" content="<?php echo Core::getInstance()->yandexwebmaster?>"/>
 
     <!-- Bootstrap core CSS -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
