a74711eaeb6d285c6688e45ac68e00d04df8de45
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-08-21 00:52:06 +0700

    Fixed crash rewrite rule

--- test/example/backend/language/id.php
+++ test/example/backend/language/id.php
@@ -38,7 +38,7 @@ $lang = [
     "rating_list" => "Urutkan film berdasarkan rating IMDB",
     "rating_key_1" => "Urutkan, film, berdasarkan, rating IMDB",
     //watch
-    "watch" => "nonton-film",
+    "watch" => "nonton-film", //If you change this then you have to edit htaccess
     "watch_keyword" => "Nonton, Film",
     "watch_header" => "Nonton Film",
     "watch_about" => "Tentang:",
