977172284a6a32cd9851ea3e219ffdcff907b01b
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-09-03 22:26:11 +0700

    - Fixed not included global meta

--- test/example/contact.php
+++ test/example/contact.php
@@ -12,16 +12,7 @@
 
     <title><?php echo Core::lang('contact_us')?> | <?php echo Core::getInstance()->title?></title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Theme CSS -->
-    <link href="css/style.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/font-circle-video.css" rel="stylesheet">
-
-    <!-- font-family: 'Hind', sans-serif; -->
-    <link href='https://fonts.googleapis.com/css?family=Hind:400,300,500,600,700|Hind+Guntur:300,400,500,700' rel='stylesheet' type='text/css'>
+    <?php include 'global-meta.php';?>
 </head>
 
 <body class="dark">
