b8a3a4d326bdb0af6324e29f948da8746b4d2667
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-09-30 00:40:03 +0700

    - Change google analytics to head section

--- test/example/global-js.php
+++ test/example/global-js.php
@@ -106,17 +106,3 @@
 	});
 </script>
 <!-- Load Options END-->
-
-<?php 
-    if (!empty(Core::getInstance()->googleanalytics)){
-		echo '<!-- Google Analytics -->
-			<script>
-				(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){
-					(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-					m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-				})(window,document,\'script\',\'https://www.google-analytics.com/analytics.js\',\'ga\');
-				ga(\'create\', \''.Core::getInstance()->googleanalytics.'\', \'auto\');
-				ga(\'send\', \'pageview\');
-			</script>';
-	}
-?>
--- test/example/global-meta.php
+++ test/example/global-meta.php
@@ -17,3 +17,17 @@
 
     <!-- font-family: 'Hind', sans-serif; -->
     <link href='https://fonts.googleapis.com/css?family=Hind:400,300,500,600,700|Hind+Guntur:300,400,500,700' rel='stylesheet' type='text/css'>
+
+    <?php 
+        if (!empty(Core::getInstance()->googleanalytics)){
+		    echo '<!-- Google Analytics -->
+			<script>
+				(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){
+					(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+					m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+				})(window,document,\'script\',\'https://www.google-analytics.com/analytics.js\',\'ga\');
+				ga(\'create\', \''.Core::getInstance()->googleanalytics.'\', \'auto\');
+				ga(\'send\', \'pageview\');
+			</script>';
+    	}
+    ?>
