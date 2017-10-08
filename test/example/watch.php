3e78b2254ef835f44348cf0f5a75c6b22cb1baa9
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-10-08 17:13:59 +0700

    - Make faster disqus load

--- test/example/watch.php
+++ test/example/watch.php
@@ -322,28 +322,38 @@
                     </div>
                     <!-- END similar videos -->';
                     if (!empty(Core::getInstance()->disqus)){
-                        echo '<!-- comments -->
+                        echo '<!-- START comments -->
                         <div class="comments">
+                            <div class="comments-block">
+                                <button id="show-comments" class="btn btn-danger btn-block" onclick="disqus();return false;">Load Comments</button>
+                            </div>
                             <div id="disqus_thread"></div>
                             <script>
-
-                                /**
-                                *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-                                *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-
-                                var disqus_config = function () {
-                                    this.page.url = \''.((Core::isHttpsButtflare()) ? 'https' : 'http') . '://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'\';  // Replace PAGE_URL with your page\'s canonical URL variable
-                                    this.page.identifier = \''.$postid.'\'; // Replace PAGE_IDENTIFIER with your page\'s unique identifier variable
-                                };
-
-                                (function() { // DON\'T EDIT BELOW THIS LINE
-                                    var d = document, s = d.createElement(\'script\');
-                                    s.src = \'https://'.Core::getInstance()->disqus.'.disqus.com/embed.js\';
-                                    s.setAttribute(\'data-timestamp\', +new Date());
-                                    (d.head || d.body).appendChild(s);
-                                })();
+                                var disqus_loaded = false;
+                                function disqus() {
+                                    if (!disqus_loaded)  {
+                                        disqus_loaded = true;
+                                        /**
+                                        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                                        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+                                        var disqus_config = function () {
+                                            this.page.url = \''.((Core::isHttpsButtflare()) ? 'https' : 'http') . '://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'\';  // Replace PAGE_URL with your page\'s canonical URL variable
+                                            this.page.identifier = \''.$postid.'\'; // Replace PAGE_IDENTIFIER with your page\'s unique identifier variable
+                                        };
+                                        var d = document, s = d.createElement(\'script\');
+                                        s.src = \'https://'.Core::getInstance()->disqus.'.disqus.com/embed.js\';
+                                        s.setAttribute(\'data-timestamp\', +new Date());
+                                        (d.head || d.body).appendChild(s);
+                                    }
+                                }
+                                //Opens comments when linked to directly
+                                var hash = window.location.hash.substr(1);
+                                if (hash.length > 8) {
+                                    if (hash.substring(0, 8) == "comment-") {
+                                        disqus();
+                                    }
+                                }
                             </script>
-                            <script id="dsq-count-scr" src="//'.Core::getInstance()->disqus.'.disqus.com/count.js" async></script>
                             <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments.</a></noscript>
                         </div>
                     <!-- END comments -->';
