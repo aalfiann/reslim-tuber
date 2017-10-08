7385009bd755c9d5b91807af70ab573da711bf64
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-10-08 14:58:05 +0700

    - Dont load disqus when not using it

--- test/example/watch.youtube.php
+++ test/example/watch.youtube.php
@@ -304,34 +304,35 @@
 
                         echo '</div>
                     </div>
-                    <!-- END similar videos -->
+                    <!-- END similar videos -->';
+                    if (!empty(Core::getInstance()->disqus)){
+                        echo '<!-- comments -->
+                        <div class="comments">
+                            <div id="disqus_thread"></div>
+                            <script>
 
-                    <!-- comments -->
-                    <div class="comments">
-                        <div id="disqus_thread"></div>
-                        <script>
+                                /**
+                                *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                                *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
 
-                            /**
-                            *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-                            *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+                                var disqus_config = function () {
+                                    this.page.url = \''.((Core::isHttpsButtflare()) ? 'https' : 'http') . '://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'\';  // Replace PAGE_URL with your page\'s canonical URL variable
+                                    this.page.identifier = \''.$postid.'\'; // Replace PAGE_IDENTIFIER with your page\'s unique identifier variable
+                                };
 
-                            var disqus_config = function () {
-                                this.page.url = \''.((Core::isHttpsButtflare()) ? 'https' : 'http') . '://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'\';  // Replace PAGE_URL with your page\'s canonical URL variable
-                                this.page.identifier = \''.$postid.'\'; // Replace PAGE_IDENTIFIER with your page\'s unique identifier variable
-                            };
-
-                            (function() { // DON\'T EDIT BELOW THIS LINE
-                                var d = document, s = d.createElement(\'script\');
-                                s.src = \'https://'.Core::getInstance()->disqus.'.disqus.com/embed.js\';
-                                s.setAttribute(\'data-timestamp\', +new Date());
-                                (d.head || d.body).appendChild(s);
-                            })();
-                        </script>
-                        <script id="dsq-count-scr" src="//'.Core::getInstance()->disqus.'.disqus.com/count.js" async></script>
-                        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments.</a></noscript>
-                    </div>
-                    <!-- END comments -->
-                </div>';
+                                (function() { // DON\'T EDIT BELOW THIS LINE
+                                    var d = document, s = d.createElement(\'script\');
+                                    s.src = \'https://'.Core::getInstance()->disqus.'.disqus.com/embed.js\';
+                                    s.setAttribute(\'data-timestamp\', +new Date());
+                                    (d.head || d.body).appendChild(s);
+                                })();
+                            </script>
+                            <script id="dsq-count-scr" src="//'.Core::getInstance()->disqus.'.disqus.com/count.js" async></script>
+                            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments.</a></noscript>
+                        </div>
+                        <!-- END comments -->';
+                    }
+                echo '</div>';
                         }
                     }
                 ?>
